4792156ccbb4502f3a4fac2d99d305b4e4eae255
junioroga <junioroga@outlook.com>
2023-05-29 13:52:09 -0300

    fist commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['universe', 'universe/shared/typescript-analysis'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+};
---
+++ src/screens/Main/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export const Main = () => {
+  return <View style={{ backgroundColor: 'blue', flex: 1 }} />;
+};
