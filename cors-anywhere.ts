7e5176a57838e08687ac97ed4c423f27efb31874
junioroga <junioroga@outlook.com>
2024-02-08 14:14:46 -0300

    feat: add cors-anywhere lib, to force cors on my animalist api to run on web

---
+++ cors-anywhere.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+// Listen on a specific host via the HOST environment variable
+const host = 'localhost'
+// Listen on a specific port via the PORT environment variable
+const port = 8080
+
+const cors_proxy = require('cors-anywhere')
+cors_proxy
+  .createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2'],
+  })
+  .listen(port, host, function () {
+    console.log(
+      'Running CORS Anywhere on ' +
+        host +
+        ':' +
+        port +
+        ' - Now you can run `yarn web` command in a new terminal.',
+    )
+  })
